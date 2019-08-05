350e039ecd6c5fe401b7934253acdfaee82a5ca3
margaretmwaura <mwauramargaret1@gmail.com>
2019-08-05 22:06:35 +0300

    Sign in with email and password

--- app/src/main/java/com/android/phaseonechallengetwo/MainActivity.java
+++ app/src/main/java/com/android/phaseonechallengetwo/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -31,7 +32,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
     private FirebaseAuth mAuth;
     GoogleSignInClient mGoogleSignInClient;
-    GoogleApiClient mGoogleApiClient;
     private static final int SIGN_IN = 9001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +62,15 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
             @Override
             public void onClick(View v) {
                 signIn();
+            }
+        });
+
+        Button signInWithEmail = (Button) findViewById(R.id.sign_in_button_with_email);
+        signInWithEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivityTwo.class);
+                startActivity(intent);
             }
         });
 
--- app/src/main/java/com/android/phaseonechallengetwo/MainActivityTwo.java
+++ app/src/main/java/com/android/phaseonechallengetwo/MainActivityTwo.java
@@ -1,22 +1,71 @@
 package com.android.phaseonechallengetwo;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 public class MainActivityTwo extends AppCompatActivity {
 
+    EditText emailTv , passwordTv;
+    String email, password;
+    Button signUp;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_two);
 
+        mAuth= FirebaseAuth.getInstance();
+
+       emailTv = (EditText) findViewById(R.id.editText_gmail);
+       passwordTv = (EditText) findViewById(R.id.editText_password);
+       signUp = (Button) findViewById(R.id.button);
+
+       signUp.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               email = emailTv.getText().toString();
+               password = passwordTv.getText().toString();
+
+               createAccount();
+           }
+
+
+       });
+
+    }
+
+    private void createAccount()
+    {
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("Acount", "account created");
+                            Intent intent = new Intent(MainActivityTwo.this,EnterDetails.class);
+                            startActivity(intent);
+                        } else
+                            {
+
+                                Log.d("Account","Account not created ");
+                        }
+
+                    }
+                });
     }
 
 }
